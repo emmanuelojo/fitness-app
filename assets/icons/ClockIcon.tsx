a2a5c14b0803d959afb1fc6635831ab15226ab73
emmanuelOjo <emmanuelojo1234@gmail.com>
2025-02-02 16:13:48 +0100

    Correct svg format implemented

--- assets/icons/ClockIcon.tsx
+++ assets/icons/ClockIcon.tsx
@@ -1,3 +1,5 @@
+import Svg, { Circle, Path } from "react-native-svg";
+
 interface Props {
   colour?: string;
   size?: number;
@@ -5,16 +7,16 @@ interface Props {
 
 export function ClockIcon({ colour = "#000000", size = 13 }: Props) {
   return (
-    <svg width={size} height={size} viewBox="0 0 12 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="5.99992" cy="7.16667" r="4.66667" stroke={colour} strokeWidth="0.875" />
-      <path d="M4.25 0.75H7.75" stroke={colour} strokeWidth="0.875" strokeLinecap="round" strokeLinejoin="round" />
-      <path
+    <Svg width={size} height={size} viewBox="0 0 12 13" fill="none">
+      <Circle cx="5.99992" cy="7.16667" r="4.66667" stroke={colour} strokeWidth="0.875" />
+      <Path d="M4.25 0.75H7.75" stroke={colour} strokeWidth="0.875" strokeLinecap="round" strokeLinejoin="round" />
+      <Path
         d="M6 4.25V7.14555C6 7.15919 6.00478 7.17241 6.01352 7.18289L7.45833 8.91667"
         stroke={colour}
         strokeWidth="0.875"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
-    </svg>
+    </Svg>
   );
 }
--- assets/icons/PlayIcon.tsx
+++ assets/icons/PlayIcon.tsx
@@ -1,3 +1,4 @@
+import Svg, { Path } from "react-native-svg";
 interface Props {
   colour?: string;
   size?: number;
@@ -5,8 +6,8 @@ interface Props {
 
 export function PlayIcon({ colour = "#191D1A", size = 12 }: Props) {
   return (
-    <svg width={size - 1} height={size} viewBox="0 0 11 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M10.3335 5.99984L0.833496 11.4847L0.833496 0.515009L10.3335 5.99984Z" fill={colour} />
-    </svg>
+    <Svg width={size - 1} height={size} viewBox="0 0 11 12" fill="none">
+      <Path d="M10.3335 5.99984L0.833496 11.4847L0.833496 0.515009L10.3335 5.99984Z" fill={colour} />
+    </Svg>
   );
 }
